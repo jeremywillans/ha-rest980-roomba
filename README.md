4b2752b1429f1e6c26f5ae7caeb3c26597983a26
Jeremy Willans <Jeremy.Willans@empired.com>
2020-01-21 16:39:42 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,130 @@
+# ha-rest980-roomba
+
+Home Assistant - iRobot Roomba i7+ Configuration using rest980
+
+This repository provides configuration to get an iRobot Roomba i7+ robot vacuum cleaner integrated with Home Assistant using the rest980 Docker Image!
+
+## Example Lovelace UI View
+
+![Lovelace Example](lovelace_example.png)
+
+**Special Cheers** 
+- ** [Facu ZAK](https://github.com/koalazak) ** for creating dorita980 and rest980 which this leverages!
+- ** [gotschi](https://community.home-assistant.io/u/gotschi/summary) ** for creating the Roomba Map PHP file which this leverages!
+
+## Step 1: Get Robot Login Details
+
+```
+docker run -it node sh -c "npm install -g dorita980 && get-roomba-password <robotIP>"
+```
+
+If you dont have direct access to Docker (such as with Hass.io) you can clone and install the dorita980 package locally (requires git and node to be installed) - refer [here](https://github.com/koalazak/dorita980#how-to-get-your-usernameblid-and-password) for instructions
+
+**Note:** Do not have iRobot App running on your phone when doing this !!!
+
+## Step 2: Configure Vacuum Map
+To allow the map to be correctly produced, you will need to create a new vacuum directory. I have chosen to put this inside the HA configuration directory, but you can choose to put this elsewhere and update the configuration accordingly (if you are using HASS and referencing my hass-addons repo, please leave this at the default!)
+
+Copy the contents of the Vacuum directory from Github into this folder.
+
+Note: The image.php file will need updating, but this will be done after the setup is complete.
+
+## Step 3: Configure Docker Compose / Run Image / Hass.io Add-on
+I use docker compose for all my HA related images, but have also listed the docker run command (copied from the rest980 github page)
+I have also included an example PHP Docker Image which i use to host the map.
+
+To allow this to work on Hass.io - I have created a custom github repository which can be added to Hass.io allowing the installation of the rest980 and nginx-php Docker Images (on amd64 systems only - as this is what the source image suports!)
+
+**DOCKER-COMPOSE**
+```
+Refer docker-compose.yaml
+```
+**Note:** I use a separate docker bridged network which can be created with:
+```
+docker network create docker
+```
+
+**DOCKER RUN**
+```
+docker run -e BLID=myuser -e PASSWORD=mypass -e ROBOT_IP=myrobotIP -e FIRMWARE_VERSION=2 -p 3000:3000 koalazak/rest980:latest
+docker run -p 3001:80 -v /<HA_CONFIG>/roomba:/app webdevops/php-nginx:latest
+```
+
+Confirm you can access the WebUI
+```
+http://<ip or fqdn of docker host>:<port>/api/local/info/state
+```
+**HASS.IO ADDON**
+
+- Add the following Github Repository to your Hass.io Add-on Store
+  https://github.com/jeremywillans/hass-addons
+- Install the rest980 addon, then update and save the configuration options 
+- Install and configure the php-nginx addon.
+- Start rest980 and php-nginx
+
+**Note**: You can also run this locally by creating a rest980 (or similar) folder within "addons/local" and then copying the config.json files from each folder on my [hass-addons](https://github.com/jeremywillans/hass-addons/) repository. 
+
+This will create a new local addon which you can install
+
+## Step 4: Get Room Details
+
+* Initiate a Clean Rooms clean from the iRobot App, ensuring you select all **BUT** one room! (if you select them all, it wont list them out. Perform a second clean on the remaining room to get all the details)
+* Navigate to ```http://<ip or fqdn of docker host>:<rest980port>/api/local/info/state```
+* Look for the "lastCommand" section and copy down the following info 
+-- pmap_id
+-- user_pmapv_id
+-- regions (as you selected all the rooms, they should all be listed here)
+
+## Step 4: Configure Home Assistant Package and Secrets
+
+I split off the ids and regions into the secrets file to make it easier to manage for future updates (as i expect these will change if you update your floorplan from the iRobot app)
+
+#### secrets.yaml
+```
+Refer secrets.yaml
+```
+
+The below is my configuration YAML file which uses the [Packages](https://www.home-assistant.io/docs/configuration/packages/) feature in HA to keep all the separate components together.
+
+I have tried to map as many of the reported statuses, however i occasionally get an "unknown" in the logs, if you work out another state, please post it up!
+
+**Note:** The input_booleans and input_text entries all start with vacuum as this is used in the templates for correct mapping in lovelace
+
+```
+Refer vacuum.yaml
+```
+
+## Step 5: Configure Lovelace
+
+I have used the below lovelace configuration with the following HACS components
+
+- lovelace-fold-entity-row
+- button-card
+
+Note: This config is taken directly from Lovelace Raw Editor as a complete view.
+
+```
+Refer lovelace.yaml
+```
+
+## Step 6: Configure Map Options
+
+After you have run a clean cycle, the map should be populating however it is unliekly not quite sized correctly.
+
+In the Roomba directory (in HA Configuration), you will need to update the height, width, offset and flip options in the image.php file to correctly reflect your layout.
+
+I have moved these as variables at the top of the file making it easier to update.
+
+**Note:** Once the vacuum has completed is clean, the image.php file references the latest.png file in the local directory so your changes wont be reflected upon refresh.
+You can use the image-dev.php file as this has been updated to only create an in-memory copy of the map each time its refreshed.
+
+## Step 7: Enjoy!
+
+## Support
+
+Got questions? Please post them [here][forum].
+
+In case you've found a bug, please [open an issue on GitHub][issue].
+
+[forum]: https://community.home-assistant.io/t/irobot-roomba-i7-configuration-using-rest980/161175
+[issue]: https://github.com/jeremywillans/ha-rest9800-roomba/issues
---
+++ vacuum/image.php
@@ -0,0 +1,202 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+
+// ADJUST THESE PARAMETERS
+$vacuum_log = 'https://<ip or fqdn of docker host>:<nginxphpport>/vacuum.log';
+$overlay_image = 'floor.png';
+$map_width = 1050;
+$map_height = 900;
+$x_offset = 220;
+$y_offset = 220;
+$flip_vertical = false;
+$flip_horizontal = false;
+$ha_rest980 = 'https://<ip or fqdn of home assistant>:<haport>/api/states/sensor.rest980';
+$ha_token = '<ha_long_live_token>';
+/////////////////
+
+if(isset($_GET['clear'])) {
+  @unlink("latest.png");
+  die();  
+}
+if(is_file("latest.png")&&!isset($_GET['last'])) {
+  header("Content-Type: image/png");
+  echo file_get_contents("latest.png");
+  die();
+}
+
+$coords = file_get_contents($vacuum_log."?v=".time());
+$coords = str_replace("(", "", $coords);
+$coords = str_replace(")", "", $coords);
+$coords = explode("\n", $coords);
+
+$date = strtotime(substr($coords[0], 42));
+
+$lastline = $coords[sizeof($coords)-2];
+$end = ["Stuck", "Finished"]; // PAUSE also available
+
+array_shift($coords);
+array_shift($coords);
+array_pop($coords);
+
+function imagelinethick($image, $x1, $y1, $x2, $y2, $color, $thick = 1)
+{
+    if ($thick == 1) {
+        return imageline($image, $x1, $y1, $x2, $y2, $color);
+    }
+    $t = $thick / 2 - 0.5;
+    if ($x1 == $x2 || $y1 == $y2) {
+        return imagefilledrectangle($image, round(min($x1, $x2) - $t), round(min($y1, $y2) - $t), round(max($x1, $x2) + $t), round(max($y1, $y2) + $t), $color);
+    }
+    $k = ($y2 - $y1) / ($x2 - $x1);
+    $a = $t / sqrt(1 + pow($k, 2));
+    $points = array(
+        round($x1 - (1+$k)*$a), round($y1 + (1-$k)*$a),
+        round($x1 - (1-$k)*$a), round($y1 - (1+$k)*$a),
+        round($x2 + (1+$k)*$a), round($y2 - (1-$k)*$a),
+        round($x2 + (1-$k)*$a), round($y2 + (1+$k)*$a),
+    );
+    imagefilledpolygon($image, $points, 4, $color);
+    return imagepolygon($image, $points, 4, $color);
+}
+
+$image = imagecreatetruecolor($map_width,$map_height);
+imagesavealpha($image, true);
+$black = imagecolorallocatealpha($image,0,0,0, 127);
+imagefill($image,0,0,$black);
+
+$overlayImage = imagecreatefrompng($overlay_image);
+imagecopy($image, $overlayImage, 0, 0, 0, 0, imagesx($overlayImage), imagesy($overlayImage));
+imagedestroy($overlayImage);
+
+$roomba = imagecreatefrompng('roomba.png');
+imagealphablending($roomba, false);
+imagesavealpha($roomba, true);
+
+foreach($coords as $i => $coord) {
+  $split = explode(",", $coord);
+  if(sizeof($split)<2) {
+    if($coord == "Stuck") {
+      $roomba_stuck = imagecreatefrompng('roomba_stuck.png');
+      imagealphablending($roomba_stuck, false);
+      imagesavealpha($roomba_stuck, true);
+      $roomba_stuck = imagerotate($roomba_stuck, $split[2]*-1, imageColorAllocateAlpha($roomba_stuck, 0, 0, 0, 127));
+      imagealphablending($roomba_stuck, false);
+      imagesavealpha($roomba_stuck, true);
+      imagecopy($image, $roomba_stuck, $oldx-10, $oldy-5, 0, 0, imagesx($roomba_stuck), imagesy($roomba_stuck));
+      imagedestroy($roomba_stuck);
+    }
+    continue;
+  }
+  
+  $part= hexdec("ff");
+  $part = round($part * $i/sizeof($coords));
+          
+  $color = imagecolorallocate($image, $part, 255, $part);
+  $x = $split[1]+$x_offset; #($split[1]+120)/2;
+  $y = $split[0]+$y_offset; #($split[0]*-1+342)/2;
+  
+  $boxsize=4;
+  $shift_y = 2;
+  $shift_x = -2;
+  
+  imagerectangle($image, $x+$shift_x, $y+$shift_y, $x+$boxsize+$shift_x, $y+$boxsize+$shift_y, $color);
+  if(isset($oldx) && isset($oldy)) {
+    imagelinethick($image, $oldx+($boxsize/2)+$shift_x, $oldy+($boxsize/2)+$shift_y, $x+($boxsize/2)+$shift_x, $y+($boxsize/2)+$shift_y, $color, 2);
+  }
+  
+  if($i+1==sizeof($coords)) {
+    if($coords[$i+1]=="Stuck") {
+      
+    }
+    else {
+      $roomba = imagerotate($roomba, $split[2]*-1, imageColorAllocateAlpha($roomba, 0, 0, 0, 127));
+      imagealphablending($roomba, false);
+      imagesavealpha($roomba, true);
+      imagecopy($image, $roomba, $x-10, $y-5, 0, 0, imagesx($roomba), imagesy($roomba));
+    }
+  }
+  
+  $oldx = $x;
+  $oldy = $y;
+}
+
+if(in_array($lastline, $end)) {
+  imagedestroy($roomba);
+  
+  if($lastline == "Stuck") {
+    $overlayImage = imagecreatefrompng('roomba_stuck.png');
+    imagealphablending($overlayImage, false);
+    imagesavealpha($overlayImage, true);
+    $color = imagecolorallocate($image, 0, 149, 223);
+    $finishedRoomba = imagerotate($overlayImage, 180, imageColorAllocateAlpha($overlayImage, 0, 0, 0, 127));
+    imagelinethick($image, $oldx+($boxsize/2), $oldy+($boxsize/2), $x+($boxsize/2)+3, $y+($boxsize/2)+10, $color, 2);
+    imagecopy($image, $overlayImage, $oldx-10, $oldy-5, 0, 0, imagesx($overlayImage), imagesy($overlayImage));
+  }
+  else if($lastline == "Finished") {
+    $overlayImage = imagecreatefrompng('roomba_charging.png');
+    imagealphablending($overlayImage, false);
+    imagesavealpha($overlayImage, true);
+    $x; # = (0+120)/2;
+    $y; # = (0*-1+330)/2;
+    $color = imagecolorallocate($image, 0, 149, 223);
+    $finishedRoomba = imagerotate($overlayImage, 180, imageColorAllocateAlpha($overlayImage, 0, 0, 0, 127));
+    imagelinethick($image, $oldx+($boxsize/2), $oldy+($boxsize/2), $x+($boxsize/2)+3, $y+($boxsize/2)+10, $color, 2);
+    imagecopy($image, $finishedRoomba, $x-5, $y+5, 0, 0, imagesx($overlayImage), imagesy($overlayImage));
+  }
+  
+}
+if($flip_vertical) {
+  imageflip( $image, IMG_FLIP_VERTICAL );
+}
+
+if($flip_horizontal) {
+  imageflip( $image, IMG_FLIP_HORIZONTAL );
+}
+
+if($lastline == "Finished") {
+  $finished=true;
+  $status="finished";
+  
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $ha_rest980);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $headers = [
+      'Authorization: Bearer '.$ha_token,
+      'Content-Type: application/json'
+  ];
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  $server_output = curl_exec ($ch);
+  curl_close ($ch);
+  $string = "";
+  $data = json_decode($server_output);
+  $battery_level = $data->attributes->batPct;
+  $string.="\nBattery: ".$battery_level."%";
+  
+}
+else if($lastline == "Stuck"){
+  $finished=false;
+  $status="stuck";
+}
+else {
+  $finished=false;
+  $status="running";
+}
+
+$dt = date('H:i:s Y-m-d', $date);
+$txt = "\nStarted: ".$dt."\n"."Status: ".$status.$string;
+$white = imagecolorallocate($image, 255, 255, 255);
+$font = "./monaco.ttf"; 
+imagettftext($image, 10, 0, 5, 15, $white, $font, $txt);
+
+header("Content-Type: image/png");
+imagepng($image);
+if(isset($_GET['last'])) {
+  imagepng($image, "latest.png");
+  imagepng($image, $date.".png");
+}
+imagedestroy($image);
+imagedestroy($roomba);
+imagedestroy($roomba_stuck);
+imagedestroy($overlayImage);
+exit;
